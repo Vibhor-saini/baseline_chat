593f68ddc488bf0f6119bc107c842b950aa68862
Vibhor-saini <theyogaphilosophy90@gmail.com>
2026-06-15 16:30:12 +0530

    fix: force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,43 +4,33 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use App\Models\Conversation;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-
+        // Force HTTPS in production
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
 
         View::composer('*', function ($view) {
-
             if (auth()->check()) {
-
                 $sentRequestsCount = Conversation::query()
-
                     ->where('status', 'pending')
-
                     ->where('user_one_id', auth()->id())
-
                     ->count();
 
                 $pendingRequestsCount = Conversation::query()
-
                     ->where('status', 'pending')
-
                     ->where('user_two_id', auth()->id())
-
                     ->count();
 
                 $view->with([
